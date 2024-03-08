e555695ff2f31a3d45c189037a1cf24936e1c862
Lee McCormick <lee.mccormick@dialexa.com>
2024-03-08 14:18:46 -0600

    added example project to swiftPackage
    - Add Project to SwiftPackageRemoteDemo folder
    - Open the project and add package to the project.
    - Import Project and show in in the new project App window

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Lee McCormick on 3/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
