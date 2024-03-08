cea78486cafc5fc219ca1f74132650a19b9905c4
Lee McCormick <lee.mccormick@dialexa.com>
2024-03-08 15:56:30 -0600

    test

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftPackageRemoteDemo",
-            exclude: ["../Example"]),
+            exclude: ["../../Example"]),
         .testTarget(
             name: "SwiftPackageRemoteDemoTests",
             dependencies: ["SwiftPackageRemoteDemo"],
