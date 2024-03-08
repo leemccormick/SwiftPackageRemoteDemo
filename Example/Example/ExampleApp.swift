59a3eb3aa7152cd7497e6c7d9b22a336f8517a11
Lee McCormick <lee.mccormick@dialexa.com>
2024-03-08 16:38:49 -0600

    got ride of exclude and unwanted files
    - don't know how to use exclude.
    - Get ride of exclude
    - Cleaned up example project.

--- Example/Example/ExampleApp.swift
+++ Example/Example/ExampleApp.swift
@@ -6,12 +6,41 @@
 //
 
 import SwiftUI
+import SwiftPackageRemoteDemo
 
+//MARK: - Example App
 @main
 struct ExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ExampleContentView()
         }
+    }
+}
+
+//MARK: - Example ContentView
+private struct ExampleContentView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, This is Example App !")
+            }
+            
+            NavigationView {
+                List {
+                    NavigationLink(destination: GreetingOnSwiftPackageRemoteView()) {
+                        Text("See Greeting View")
+                    }
+                    
+                    NavigationLink(destination: SuccessView(message: "Show success in Example")){
+                        Text("See Success View")
+                    }
+                }
+            }
+        }
+        .padding()
     }
 }
